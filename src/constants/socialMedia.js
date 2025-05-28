3434155b9fe52227dc21833bc2f1af3d1e550629
Sol <solzito.1889@gmail.com>
2025-05-28 13:08:47 -0300

    se corrigen redes

--- src/constants/socialMedia.js
+++ src/constants/socialMedia.js
@@ -1,35 +1,19 @@
-import {
-    AiFillGithub,
-    AiFillInstagram,
-    AiFillLinkedin,
-    AiFillMail,
-    AiFillTwitterCircle
-} from "react-icons/all";
+import { BsGithub, BsLinkedin, BsMailbox } from "react-icons/bs";
 
 export const SOCIAL_MEDIA = [
-    {
-        id: "social-media-1",
-        icon: AiFillLinkedin,
-        link: "https://www.linkedin.com/in/sudhanva-nadiger-b616a6212/",
-    },
-    {
-        id: "social-media-2",
-        icon: AiFillGithub,
-        link: "https://www.github.com/Sudhanva-Nadiger",
-    },
-    {
-        id: "social-media-3",
-        icon: AiFillMail,
-        link: "mailto:sudhanvanadiger12@gmail.com",
-    },
-    {
-        id: "social-media-4",
-        icon: AiFillInstagram,
-        link: "https://www.instagram.com/sudhanva_nadiger__",
-    },
-    {
-        id: "social-media-5",
-        icon: AiFillTwitterCircle,
-        link: "https://twitter.com/SudhanvaNadiger",
-    },
+  {
+    id: "social-media-1",
+    icon: BsLinkedin,
+    link: "https://www.linkedin.com/in/solzito1998/",
+  },
+  {
+    id: "social-media-2",
+    icon: BsGithub,
+    link: "https://github.com/Sol-Zito",
+  },
+  {
+    id: "social-media-3",
+    icon: BsMailbox,
+    link: "mailto:solzito.1889@gmail.com",
+  },
 ];

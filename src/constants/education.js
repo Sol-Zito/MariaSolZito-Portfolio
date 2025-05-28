0019996a15fb58b5bf5a5a1fca0b5d7df68d9000
Sol <solzito.1889@gmail.com>
2025-05-28 13:10:06 -0300

    correccion en educacion

--- src/constants/education.js
+++ src/constants/education.js
@@ -1,14 +1,23 @@
-
-import nitk from '../assets/images/Education/nitk.png';
-
 export const EDUCATION_LIST = [
   {
     id: "education-1",
-    icon: nitk,
-    title: "National Institute of Technology Karnataka, Surathkal",
-    degree: "Bachelor of Technology",
-    duration: "December 2020 - April 2024",
-    content1: "Major: Mechanical Engineering",
-    content2: "Minor: Information Technology",
+    icon: "",
+    title: "Digital House",
+    degree: "Front End Specialization",
+    duration: "July 2023 - July 2024",
+  },
+  {
+    id: "education-2",
+    icon: "",
+    title: "Digital House",
+    degree: "Full Stack Developer",
+    duration: "Oct 2021 - July 2023",
+  },
+  {
+    id: "education-3",
+    icon: "",
+    title: "Soldados Argentinos",
+    degree: "High School",
+    duration: "Mar 2011 - Dec 2016",
   },
 ];

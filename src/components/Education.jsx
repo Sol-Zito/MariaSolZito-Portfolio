0493eba41feb4314ec1b26bd313e08c0aab0ae8a
Sol <solzito.1889@gmail.com>
2025-05-28 13:14:10 -0300

    correccion en componente de educacion

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -1,10 +1,8 @@
-
 import { EDUCATION_LIST } from "../constants";
 import Lottie from "react-lottie";
 import animationData from "../assets/images/Education/login.json";
 import { motion } from "framer-motion";
 
-// lottie config
 const defaultOptions = {
   loop: true,
   autoplay: true,
@@ -15,35 +13,25 @@ const defaultOptions = {
 };
 
 const FeatureCard = (props) => {
-  const { icon, title, degree, duration, content1, content2, index } = props
-  return <div
-    className={`flex sm:flex-row flex-col p-4 rounded-[20px]
-	${index === EDUCATION_LIST.length - 1 ? "mb-0" : "mb-6"} feature-card`}
-  >
+  const { title, degree, duration, index } = props;
+  return (
     <div
-      className={`w-[64px] h-[64px] rounded-full items-center justify-center`}
+      className={`flex sm:flex-row flex-col p-4 rounded-[20px]
+	${index === EDUCATION_LIST.length - 1 ? "mb-0" : "mb-6"} feature-card`}
     >
-      {/* <img src="" alt="National Institute Of Technology, Karnataka@seekpng.com" className="bg-blue"></img> */}
-      <img src={icon} alt="icon" className="w-[80%] h-[80%] ml-8 sm:ml-auto object-contain" />
+      <div className="flex-1 flex flex-col ml-4">
+        <h4 className="font-semibold text-dark_primary text-[20px] leading-[30px] mb-1 text-gradient2">
+          {degree}
+        </h4>
+        <p className="font-normal text-dark_primary text-[16px] leading-[30px] mb-1 ">
+          {title}
+        </p>
+        <p className="font-poppins text-light_secondary font-normal text-[14px] leading-[30px] mb-1">
+          {duration}
+        </p>
+      </div>
     </div>
-    <div className="flex-1 flex flex-col ml-4">
-      <h4 className="font-semibold text-dark_primary text-[20px] leading-[30px] mb-1 text-gradient2">
-        {title}
-      </h4>
-      <p className="font-normal text-dark_primary text-[16px] leading-[30px] mb-1 ">
-        {degree}
-      </p>
-      <p className="font-poppins text-light_secondary font-normal text-[14px] leading-[30px] mb-1">
-        {duration}
-      </p>
-      <p className="font-poppins text-light_secondary font-normal  text-[16px] leading-[30px] mb-1">
-        - {content1}
-      </p>
-      <p className="font-poppins text-light_secondary font-normal text-[16px] leading-[30px] mb-1">
-        - {content2}
-      </p>
-    </div>
-  </div>
+  );
 };
 
 const Education = () => {
@@ -51,7 +39,9 @@ const Education = () => {
     <section id="education" className="bg-primaryLinear pt-4">
       <motion.h1
         whileInView={{ y: [-20, 0], opacity: [0, 1] }}
-        transition={{ duration: 1, type: 'spring', stiffness: "120" }} className="flex-1 font-Poppins font-semibold ss:text-[55px] sm:text-[45px] sm:px-16 px-5 ss:leading-[80px] leading-[80px]">
+        transition={{ duration: 1, type: "spring", stiffness: "120" }}
+        className="flex-1 font-Poppins font-semibold ss:text-[55px] sm:text-[45px] sm:px-16 px-5 ss:leading-[80px] leading-[80px]"
+      >
         Education
       </motion.h1>
       <motion.div
@@ -63,8 +53,7 @@ const Education = () => {
           <Lottie options={defaultOptions} />
         </div>
 
-
-        <div className={`flex-1 flex justify-center items-start flex-col`}>
+        <div className={`flex-1 flex flex-col justify-center items-start`}>
           {EDUCATION_LIST.map((feature, index) => (
             <FeatureCard key={feature.id} index={index} {...feature} />
           ))}

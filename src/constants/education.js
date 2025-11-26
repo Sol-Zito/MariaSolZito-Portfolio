c5166bba8d1045846b2befe0bb68cd4d1442139a
Sol Zito <solzito.1889@gmail.com>
2025-11-26 11:09:44 -0300

    Systems Analyst Polytechnic

--- src/constants/education.js
+++ src/constants/education.js
@@ -2,19 +2,26 @@ export const EDUCATION_LIST = [
   {
     id: "education-1",
     icon: "",
+    title: "Polytechnic Institute 'Gral. San Martin` (UNR)",
+    degree: "Systems Analyst",
+    duration: "Feb 2026 - Dec 2029",
+  },
+  {
+    id: "education-2",
+    icon: "",
     title: "Digital House",
     degree: "Front End Specialization",
     duration: "July 2023 - July 2024",
   },
   {
-    id: "education-2",
+    id: "education-3",
     icon: "",
     title: "Digital House",
     degree: "Full Stack Developer",
     duration: "Oct 2021 - July 2023",
   },
   {
-    id: "education-3",
+    id: "education-4",
     icon: "",
     title: "Soldados Argentinos",
     degree: "High School",
